5c5cb1016a58dfa3aa2a9aefbd03e5402a369585
matt-mcalister <matt.mcalister93@gmail.com>
2018-11-27 15:09:40 -0500

    wrote and passed tests for validating dog username

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,4 +3,6 @@ class Dog < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: true, length: { minimum: 8 }
 end
--- spec/models/dog_spec.rb
+++ spec/models/dog_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Dog, type: :model do
+  it { should validate_presence_of(:username) }
+  it { should validate_uniqueness_of(:username) }
+  it do
+       should validate_length_of(:username).
+       is_at_least(8).
+       on(:create)
+   end
+
 end
