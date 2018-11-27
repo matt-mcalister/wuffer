d4e5f7016227c1d145430d6380d1fb66377a4811
matt-mcalister <matt.mcalister93@gmail.com>
2018-11-27 14:56:04 -0500

    setup shoulda-matcher

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
 require 'support/factory_bot'
+require 'support/shoulda'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
