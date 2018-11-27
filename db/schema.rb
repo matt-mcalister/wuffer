afb1deeebb4188073068e72cadf06069498afaa7
matt-mcalister <matt.mcalister93@gmail.com>
2018-11-27 14:39:57 -0500

    setup dog model with devise, only base specs

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20181127192000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dogs", force: :cascade do |t|
+    t.string   "username"
+    t.string   "image_url"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_dogs_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_dogs_on_reset_password_token", unique: true, using: :btree
+  end
 
 end
---
+++ spec/factories/dogs.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :dog do
+    username { "stella_pup" }
+    image_url { "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12235916/Border-Terrier-On-White-03.jpg" }
+  end
+end
