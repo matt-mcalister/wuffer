e065bd90066ae5bc71187eecf14a6545f8c745fc
matt-mcalister <matt.mcalister93@gmail.com>
2018-11-29 15:17:49 -0500

    updated routes to remove edit password

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :dogs
+
+  devise_for :dogs, skip: [:passwords], path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
