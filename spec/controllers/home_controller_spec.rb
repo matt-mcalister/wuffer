55d690d8836a597815cbc33f5ef06d7ab8dfe685
matt-mcalister <matt.mcalister93@gmail.com>
2018-11-30 10:19:14 -0500

    added support for devise in rspec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
 
-  describe "GET #index" do
+  describe "GET #landing" do
     it "returns http success" do
-      get :index
+      get :landing
       expect(response).to have_http_status(:success)
     end
   end
