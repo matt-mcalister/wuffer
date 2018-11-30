34debff1bf02cac772e5ca59a93d37bdf1263f1a
matt-mcalister <matt.mcalister93@gmail.com>
2018-11-30 10:03:44 -0500

    removed unnecessary initialize from ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,28 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_dog!, except: [:signup, :login, :landing]
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
-  # def initialize
-  #   super
-  #   puts "******************************"
-  #   puts "******************************"
-  #   puts "******************************"
-  #   puts "******************************"
-  #   puts "******************************"
-  #   puts "******************************"
-  #   puts "******************************"
-  #   puts "******************************"
-  #   puts "yooooooooooooooooooooooooooooo"
-  #   byebug
-  #   puts "******************************"
-  #   puts "******************************"
-  #   puts "******************************"
-  #   puts "******************************"
-  #   puts "******************************"
-  #   puts "******************************"
-  #   puts "******************************"
-  #   puts "******************************"
-  # end
-
+  
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
