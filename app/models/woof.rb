83dee236c214b9bd1a95750b21d24ba23beb87e0
matt-mcalister <matt.mcalister93@gmail.com>
2018-12-03 16:20:45 -0500

    passed basic tests for woof

--- app/models/woof.rb
+++ app/models/woof.rb
@@ -1,4 +1,24 @@
 class Woof < ApplicationRecord
   belongs_to :dog
   belongs_to :rewoof, class_name: "Woof", optional: true
+  validate :no_content, :too_much_content
+
+  def original_dog
+    rewoof ? rewoof.dog : dog
+  end
+
+  private
+  def no_content
+    if text.nil? && rewoof_id.nil?
+      errors.add(:rewoof_id, "must exist if there is no text")
+      errors.add(:text, "must exist if it is not a rewoof")
+    end
+  end
+
+  def too_much_content
+    if !text.nil? && !rewoof_id.nil?
+      errors.add(:rewoof_id, "cannot exist if there is text")
+      errors.add(:text, "cannot exist if it is a rewoof")
+    end
+  end
 end
--- spec/models/woof_spec.rb
+++ spec/models/woof_spec.rb
@@ -6,7 +6,8 @@ RSpec.describe Woof, type: :model do
   it "raises an error if it contains no text or rewoof_id" do
     woof = FactoryBot.build_stubbed(:no_content)
     woof.valid?
-    woof.errors.full_messages.should include("Woof must contain either text or a rewoof_id")
+    woof.errors.full_messages.should include("Rewoof must exist if there is no text")
+    woof.errors.full_messages.should include("Text must exist if it is not a rewoof")
   end
 
   it "has a rewoof_id if it has no text" do
@@ -26,7 +27,8 @@ RSpec.describe Woof, type: :model do
   it "raises an error if it has both text and a rewoof_id" do
     woof = FactoryBot.build_stubbed(:too_much_content)
     woof.valid?
-    woof.errors.full_messages.should include("Woof must not have both text and a rewoof_id")
+    woof.errors.full_messages.should include("Rewoof cannot exist if there is text")
+    woof.errors.full_messages.should include("Text cannot exist if it is a rewoof")
   end
 
 
@@ -34,7 +36,13 @@ RSpec.describe Woof, type: :model do
     dog = FactoryBot.build_stubbed(:dog)
     original_woof = FactoryBot.build_stubbed(:woof, dog: dog)
     rewoof = FactoryBot.build_stubbed(:rewoof, rewoof: original_woof)
-    rewoof.orginal_dog.should eq(dog)
+    rewoof.original_dog.should eq(dog)
+  end
+
+  it "should respond to the method 'orginal_dog', which returns the author dog if it is not a rewoof" do
+    dog = FactoryBot.build_stubbed(:dog)
+    woof = FactoryBot.build_stubbed(:woof, dog: dog)
+    woof.original_dog.should eq(dog)
   end
 
 end
